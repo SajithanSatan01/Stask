13271ca57343c8ae2df88d5d20682576b4e855fb
SAJITHAN SATAN <Sajithansatan@gmail.com>
2026-03-03 15:21:07 +0530

    satan

---
+++ task.js
@@ -0,0 +1,35 @@
+let a=20;
+let b=5;
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+let salary = 10000;
+
+salary +=2000;
+console.log(salary);
+
+salary -= 1500;
+console.log(salary)
+
+salary *=2;
+console.log(salary)
+
+let age=18;
+let indiancitizen=true;
+
+console.log(age<20 && age<=18)
+
+let number = -5;
+let result =number >= 0? "positive":"negative";
+console.log(result)
+
+let num1=10;
+let num2=20;
+if(num1>num2){
+    console.log(num1+"is greater");
+}else{
+    console.log(num2+ "is greater");
+}
